93cf326fa6f8270e11ab300002d875019c8cf111
Otavio <otavio.tuness@gmail.com>
2023-07-24 22:26:29 -0300

    Create connection db, finish create domain and tests

---
+++ encoder/domain/video_test.go
@@ -0,0 +1,28 @@
+package domain_test
+
+import (
+	"encoder/domain"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidateIfVideoIsEmpty(t *testing.T) {
+	video := domain.NewVideo()
+	err := video.Validate()
+
+	require.Error(t, err)
+}
+
+func TestVideoIdIsNotAUuid(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "abc"
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Error(t, err)
+}
