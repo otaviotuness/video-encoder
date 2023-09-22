cba810605208eda82282769f94668cfa00a77b70
Otavio Tunes <Otavio.Tunes-ext@ab-inbev.com>
2023-09-22 14:04:31 -0300

    Inclindo test e upload manager

--- encoder/application/services/upload_manager.go
+++ encoder/application/services/upload_manager.go
@@ -3,8 +3,10 @@ package services
 import (
 	"context"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 
 	"cloud.google.com/go/storage"
@@ -70,4 +72,56 @@ func getClientUpload() (*storage.Client, context.Context, error) {
 	}
 
 	return client, ctx, nil
+}
+
+func (vu *VideoUpload) ProcessUpload(concurrency int, doneUpload chan string) error {
+
+	//vai gerar um buffer com a quantidade de canais que a máquina tem
+	in := make(chan int, runtime.NumCPU())
+	//que avisar que terminou o upload
+	returnChannel := make(chan string)
+
+	err := vu.loadPaths()
+	if err != nil {
+		return err
+	}
+
+	uploadClient, ctx, err := getClientUpload()
+	if err != nil {
+		return err
+	}
+
+	//verifica quantos workers serão necessários
+	for process := 0; process < concurrency; process++ {
+		go vu.uploadWorker(in, returnChannel, uploadClient, ctx)
+	}
+
+	//criar uma goroutine para ficar escutando o canal de retorno
+	go func() {
+		for x := 0; x < len(vu.Paths); x++ {
+			in <- x
+		}
+	}()
+
+	for r := range returnChannel {
+		if r != "" {
+			doneUpload <- r
+			break
+		}
+	}
+	return nil
+}
+
+func (vu *VideoUpload) uploadWorker(in chan int, returnChan chan string, uploadClaint *storage.Client, ctx context.Context) {
+	//range no canal de entrada
+	for x := range in {
+		error := vu.UploadObject(vu.Paths[x], uploadClaint, ctx)
+		if error != nil {
+			vu.Errors = append(vu.Errors, vu.Paths[x])
+			log.Printf("error during the upload: %v. Error: %v", vu.Paths[x], error)
+			returnChan <- error.Error()
+		}
+
+		returnChan <- ""
+	}
 }
